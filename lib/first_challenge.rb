5effddd9ab8ab52c8836ed7f5b9917d186ded50f
HARPREET GHOTRA <harpreetghotra40@icloud.com>
2019-05-08 22:55:09 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,3 +1,20 @@
 def first_challenge
-   epic_tragedy = {}
+   epic_tragedy = {
+     :montague =>{
+       :patriarch => {
+         :name => "Lord Montague",
+                },
+       :matriarch => {
+         :name => "Lady Montague",
+                }
+     },
+     :capulet =>{
+       :patriarch => {
+         :name => "Lord Capulet",
+                },
+       :matriarch => {
+         :name => "Lady Capulet",
+                }
+     }
+   }
 end
